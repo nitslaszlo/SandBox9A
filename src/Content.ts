54f0f5362ddf61dc20d85f8bcc0eedc55312ff40
git <nitslaszlo@gmail.com>
2020-04-03 10:54:34 +0200

    up

--- src/Content.ts
+++ src/Content.ts
@@ -25,10 +25,41 @@ export default class Content {
 
         // Kezd a kódolást innen -->
 
-        res.write("Hello Jedlik, hello 9.A");
-        res.write("<h2 style='color:blue;'>TipeScript</h2>");
-        res.write("próba\n");
-        res.write("alma\n");
+        // Az  algoritmusokban  a  változók  a  megadott  típusú
+        // értékek  (adatok)tárolására  használt memóriatartományok elnevezései.
+        // let x = 12; // változó definíció: let változóAzon = kezdőérték
+        // x = 20;
+        // res.write(`Az x változó érték: ${x}\n`);
+        // res.write(`${x}\n`);
+        // res.write(x.toString() + "\n");
+        // const szöveg = "alma";
+        // // szöveg = "körte";
+        // res.write(szöveg + "\n");
+        // let esik: boolean;
+        // esik = true;
+        // esik = false;
+        // res.write(`${!esik}\n`);
+
+        res.write("Téglalp területe és kerülete\n");
+        res.write("a= ");
+        let oldalA: number = parseInt(params.inputa as string);
+        if (isNaN(oldalA)) {
+            oldalA = 20;
+        }
+        res.write(`<input type='text' name='inputa' value=${oldalA} style='width:5em;' onChange='this.form.submit();'>\n`);
+
+        res.write("b= ");
+        let oldalB: number = parseInt(params.inputb as string);
+        if (isNaN(oldalB)) {
+            oldalB = 30;
+        }
+        res.write(`<input type='text' name='inputb' value=${oldalB} style='width:5em;' onChange='this.form.submit();'>\n`);
+
+        const terület = oldalA * oldalB;
+        const kerület = 2 * (oldalA + oldalB);
+        res.write(`Terület = ${terület}\n`);
+        res.write(`Kerület = ${kerület}\n`);
+        res.write("\n\n");
 
         // <---- Fejezd be a kódolást
 
