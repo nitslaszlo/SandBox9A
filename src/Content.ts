e0eed6c7b6b21f1ea460837df0caef3fd6382b8d
DanProgramer <judalion777@gmail.com>
2020-05-22 17:01:40 +0200

    Clean up

--- src/Content.ts
+++ src/Content.ts
@@ -1,45 +1,6 @@
 ﻿import fs from "fs";
 import http from "http";
 import url from "url";
-
-// F1.: Készíts függvényt, ami meghatározza (visszatér) egy szám számjegyeinek összegével! A számot paraméterben kapja a függvény!
-// A függvényt teszteljed tetszőleges hívással!
-
-// F2.: Készíts függvényt, ami igaz értékkel tér vissza, ha egy számot maradék nélkül oszt el egy másik szám, hamissal ha nem osztja!
-// A számot és az osztót paraméterekben kapja a függvény!
-// A függvényt teszteljed tetszőleges hívással!
-
-// F3.: Készíts függvényt, ami meghatározza egy szám osztóinak a darabszámát!  A számot paraméterben kapja a függvény!
-// A függvényt teszteljed tetszőleges hívással!
-
-// F4.: Készíts függvényt, ami igaz értékkel tér vissza, ha a szám prím, hamissal, ha nem prím! A számot paraméterben kapja a függvény!
-// A függvényt teszteljed tetszőleges hívással!
-
-// F5.: Készíts függvényt, amivel egy sugár paramétrből a kör kerületét tudod meghatározni!
-// Ha az r paraméter r <= 0, vagy r = NaN, vagy r = undefined, akkor NaN értékkel térjen vissza!
-
-// F6.: Készíts függvényt, amivel egy sugár paramétrből a kör területét tudod meghatározni!
-// Ha az r paraméter r <= 0, vagy r = NaN, vagy r = undefined, akkor NaN értékkel térjen vissza!
-
-// F7.: Készíts függvényt, ami az "ax +  b = 0" egyenlet gyökét (x) határozza meg! A függvény paraméterei az "a" és "b" értéke legyen!
-// A függvényt teszteljed tetszőleges hívással!
-
-// F8.: LNKO meghatározása
-function LNKO(a: number, b: number): number {
-    while (a !== b) {
-        if (a > b) {
-            a = a - b;
-        } else {
-            b = b - a;
-        }
-    }
-    return a;
-}
-
-// F9.: LKKT
-function LKKT(sz1: number, sz2: number): number {
-    return (sz1 * sz2) / LNKO(sz1, sz2);
-}
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
         // favicon.ico kérés kiszolgálása:
@@ -62,22 +23,6 @@ export default class Content {
         const params = url.parse(req.url as string, true).query;
 
         // Kezd a kódolást innen -->
-
-        // Gy1.: Készíts programot ami két szám legkisebb közös többszörösét (LKKT) határozza meg és írja ki!
-        // Az algoritmust a neten megtalálod!
-        res.write(`LNKO(12,20) = ${LNKO(12, 20)}\n`);
-        res.write(`LKKT(7,13) = ${LKKT(7, 13)}\n`);
-
-        for (const i of "alma") {
-            res.write(`${i}\n`);
-        }
-        const szöveg: string = "körte";
-        for (let i = 0; i < szöveg.length; i++) {
-            res.write(`${szöveg[i]}\n`);
-        }
-
-        // Gy2.: Definiálj egy kör sugarát! Határozd meg a megadott sugarú kör kerületét és területét! (F5.-F6. függvények felhasználásával)
-
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
