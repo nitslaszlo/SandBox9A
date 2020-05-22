266f207e07f116ecb7705e3e546e0f685f80e778
sandorlaszlo <sandor.laszlo@jedlik.eu>
2020-05-22 11:17:55 +0200

    F7 - F10

--- src/Content.ts
+++ src/Content.ts
@@ -3,12 +3,21 @@ import http from "http";
 import url from "url";
 
 // Feladatok
+
+//Minden függvény működését tesztelje is le tetszőleges paraméterekkel!
+
 // F1.: Írjon függvényt, amely paraméterben kapja egy háromszög 3 oldalát és visszaadja annak kerületét!
 // F2.: Írjon függvényt, amely paraméterben kapja egy háromszög 3 oldalát és visszaadja annak területét! (Ehhez nézzen utána a Héron-képletnek!)
 // F3.: Írjon függvényt, amely paraméterben egy számokat tartalmazó vektort kap és visszaadja a számok átlagát!
 // F4.: Írjon függvényt, amely paraméterben egy számokat tartalmazó vektort kap és logikai értékkel (boolean) tér vissza, amely true ha van a számok között negatív szám és false ha nincs!
 // F5.: Írjon függvényt, amely paraméterben egy sztingeket tartalmazó vektort kap, és kiírja a vektor elemeit egymás alá!
 // F6.: Írjon függvényt, amely paraméterben két téglalap a és b oldálát kapja, és true értékkel tér vissza ha a két téglalap hasonló, false-szal ha nem! (Két téglalap hasonló, ha az oldalaik aránya egyenlő.)
+
+// F7.: Írjon függvényt, amely paraméterben kap egy sztringet, és egy karaktert, és visszaadja a sztringben lévő karakterek számát!
+// F8.: Írjon függvényt, amely paraméterben kap két számot és visszaadja a két szám mértani közepét!
+// F9.: Írjon függvényt, amely paraméterben kapja egy derékszögű háromszög két befogóját és visszaadja az átfogóját!
+// F10.: Írjon függvényt, amely egy paraméterben kapott számot eloszt 2-vel annyiszor, ahányszor lehet és közben kiírja a számot a kettes számok szorzataként megszorozva egy olyan számmal, amely már nem osztható 2-vel. Pl.: 120 = 2*2*2*15. A függvény ne adjon vissza semmit!
+
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
         // favicon.ico kérés kiszolgálása:
